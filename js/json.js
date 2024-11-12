a3551d7d4e195cda2e20dd3b3945d65d6f0d1282
NibrasKhalid <156461760+NibrasKhalid@users.noreply.github.com>
2024-11-12 10:11:46 -0500

    commit3

--- js/json.js
+++ js/json.js
@@ -8,37 +8,38 @@ async function populate(params) {
 
     // Introducing JavaScript Object Notation (JSON): https://json.org/
     // STEP 4: Store the URL of a JSON file in a variable */
-    const requestUrl = "";
+    const requestURL = "";
     // STEP 5: Use the new URL to create a new request object
-    
+    const request = new Request(requestURL);
     // STEP 6: Make a network request with the fetch() function, which returns a Response object
-    
+    const response = await fetch(request);
     // STEP 7: Capture the returned Response object and covert to a JSON object using json()
-    
+    const iScream = await response.json();
     // STEP 8: Output the iScream JSON object to the console 
-    
+    console.log(iScream);
     // STEP 9a: Invoke the populateHeader function here, then build it below
-    
+    populateHeader(iScream);
     // STEP 10a: Invoke the showTopFlavors function here, then build it below
-    
+    showTopFlavors(iScream);
 
 }
 // STEP 3b: Call the populate() function
 
-
+populate();
 /* STEP 9b: Build out the populateHeader() function */
-function populateHeader() {
+function populateHeader(jsonObj) {
     // Create the H1 element
-    
+    const headrH1 = document.createElement("h1");
     // Grab the company name from the JSON object and use it for the text node
-    
+    headrH1.textContent = jsonObj.companyName;
     // Inject the complete H1 element into the DOM, inside the HEADER
-    
+    header.append(headrH1);
 };
 /* STEP 10b: Assemble the showTopFlavors() function */
 function showTopFlavors() {
     // STEP 10c: Attache the JSON topFlavors object to a variable
-    //let topFlavors = jsonObj.topFlavors;
+    let topFlavors = jsonObj.topFlavors;
+    console.log(topFlavors);
     // STEP 10d: Loop through the topFlavors object
     for (let i = 0; i < topFlavors.length; i ++) {
         // STEP 10e: build HTML elements for the content
