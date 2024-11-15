4a4921d291e4e368370ff60be748934b2b741ad3
NibrasKhalid <156461760+NibrasKhalid@users.noreply.github.com>
2024-11-14 21:32:17 -0500

    Checking link

--- js/json.js
+++ js/json.js
@@ -8,7 +8,8 @@ async function populate() {
 
     // Introducing JavaScript Object Notation (JSON): https://json.org/
     // STEP 4: Store the URL of a JSON file in a variable */
-    const requestURL = "https://nibraskhalid.github.io/COMP1073Week10/js/i-scream.json";
+    const requestURL = "https://aaliyah033.github.io/lesson-10/js/i-scream.json";
+    // const requestURL = "https://nibraskhalid.github.io/COMP1073Week10/js/i-scream.json";
     // STEP 5: Use the new URL to create a new request object
     const request = new Request(requestURL);
     // STEP 6: Make a network request with the fetch() function, which returns a Response object
@@ -49,7 +50,9 @@ function showTopFlavors(jsonObj) {
     let ul = document.createElement("ul");
         // STEP 10f: Set the textContent property for each of the above elements (except the UL), based on the JSON content
         h2.textContent = topFlavors[i]["name"];
-        image.setAttribute("src", "https://nibraskhalid.github.io/COMP1073Week10/images/" + topFlavors[i].image);
+        image.setAttribute("src", "https://aaliyah033.github.io/lesson-10/images/" + topFlavors[i].image);
+        // image.setAttribute("src", "https://nibraskhalid.github.io/COMP1073Week10/images/" + topFlavors[i].image);
+
         let ingredients = topFlavors[i].ingredients
         // STEP 10g: Build a loop for the ingredients array in the JSON
         for(let j=0;j<ingredients.length;j++){
